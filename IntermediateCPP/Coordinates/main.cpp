9bfbcf82cb406797c72772c020f754059cedc4e0
JoMayfield <156383560+JoMayfield@users.noreply.github.com>
2025-02-08 13:35:08 -0600

    Create main.cpp

---
+++ IntermediateCPP/Coordinates/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Coordinate.hpp"
+using namespace std;
+
+int main()
+{
+    Coordinate c1 = Coordinate();
+    c1.x = 1;
+    c1.printCoordMethod();
+    
+    return 0;
+}
